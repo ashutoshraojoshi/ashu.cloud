8309b3d49e24366fb40fd1b1aa49e6db5054e17b
Ashutosh Joshi <ashutoshraojoshi@gmail.com>
2023-09-06 13:09:59 +0530

    updated v1.2

--- ashu_cloud_test01/settings.py
+++ ashu_cloud_test01/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'chatbot.apps.ChatbotConfig',
     'index.apps.IndexConfig',
-    'registration.apps.RegistrationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
